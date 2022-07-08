396c1128d54ac18dd580bbcc8650c8288b030f76
paulchainkang <paul@paul-kang.com>
2022-07-08 12:32:44 +0800

    add atom code and visualtft projects

---
+++ VisualTFT-projects/lower_limb/output/DefineID.h
@@ -0,0 +1,38 @@
+//#define  _SCREEN_START                                                         0
+
+//#define  _SCREEN_RFID                                                          1
+
+//#define  _SCREEN_LOWER_LIMB                                                    2
+
+//#define  _SCREEN_EXAM_START                                                    3
+
+//#define  _SCREEN_EXAM_FINISH                                                   4
+
+//#define  _SCREEN_UPLOAD_FAILED                                                 5
+
+//#define  _SCREEN_SCREEN6                                                       6
+
+//#define  _SCREEN_SCREEN7                                                       7
+
+//#define  _SCREEN_SCREEN8                                                       8
+
+//#define  _SCREEN_SCREEN9                                                       9
+
+//#define  _SCREEN_SCREEN10                                                     10
+
+#define  _BTN_START_BUTTON1                                                    1
+
+#define  _BTN_RFID_BUTTON1                                                     1
+
+#define  _BTN_RFID_BUTTON2                                                     2
+
+#define  _BTN_LOWER_LIMB_BUTTON1                                               1
+
+#define  _RTC_EXAM_START_RTC1                                                  1
+
+#define  _RTC_EXAM_START_RTC2                                                  2
+
+#define  _BTN_EXAM_FINISH_BUTTON1                                              1
+
+#define  _BTN_EXAM_FINISH_BUTTON2                                              2
+
